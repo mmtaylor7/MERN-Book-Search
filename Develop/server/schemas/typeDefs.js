290c8d500b47047c76529b168c6b8d87ba975eb5
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-24 10:40:04 -0400

    mutation and queries

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -20,7 +20,18 @@ const typeDefs = gql`
 
   type Auth {
     token: Id!
-    user: user
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
