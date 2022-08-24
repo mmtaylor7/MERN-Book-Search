4a98dad73a2eb61b2d661523d374ccc41be1b3d8
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-24 16:01:09 -0400

    queries.js getme

---
+++ server/utils/queries.js
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const QUERY_MATCHUPS = gql`
+  query matchups($_id: String) {
+    matchups(_id: $_id) {
+      _id
+      tech1
+      tech2
+      tech1_votes
+      tech2_votes
+    }
+  }
+`;
