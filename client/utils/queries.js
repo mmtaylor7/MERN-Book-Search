28ddbcd240de20b9ed6cf7fedad9e2fd3a59357f
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-25 18:29:51 -0400

    removed class content from WS

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -18,15 +18,3 @@ export const GET_ME = gql`
     }
   }
 `;
-
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
