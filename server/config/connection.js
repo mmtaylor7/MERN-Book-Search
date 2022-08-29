03d272c24b9679bee9cc6d2efa290ec2b5ee5266
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-29 09:01:12 -0400

    connected to atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://phimu:Revolution9@cluster0.ys8cpey.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  () => console.log(" Mongoose is connected")
+);
 
 module.exports = mongoose.connection;
