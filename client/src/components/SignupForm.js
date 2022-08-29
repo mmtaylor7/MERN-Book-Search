708957e63fe9e9f8c2ecfa03f139cc37e8ed16b2
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-28 23:19:58 -0400

    changed response&createUser to data/add user

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -33,7 +33,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const data = await addUser(userFormData);
 
       if (!response.ok) {
         throw new Error("something went wrong!");
