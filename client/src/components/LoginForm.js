7d426773a883a79f326d57b0af08dadbb152e4e9
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-29 00:26:54 -0400

    fixed utils folder

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutation";
+import { LOGIN_USER } from "../utils/mutations";
 
 import { loginUser } from "../utils/API";
 import Auth from "../utils/auth";
