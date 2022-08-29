e33c217931c6cf1fcbaed5d2bf43f7b1c1d686d9
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-29 00:30:07 -0400

    fixed add user call

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -9,6 +9,10 @@ export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
