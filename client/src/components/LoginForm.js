69d4fdb3428a56b67f8af2eae39f65a4575d9e00
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-29 20:03:38 -0400

    await login takes in 'variables'

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -14,6 +14,14 @@ const LoginForm = () => {
 
   const [login, { error }] = useMutation(LOGIN_USER);
 
+  useEffect(() => {
+    if(error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error])
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -30,7 +38,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await login(userFormData);
+      const response = await login( { variables: {...userFormData});
 
       if (!response.ok) {
         throw new Error("something went wrong!");
